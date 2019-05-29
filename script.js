062644bbc17a62eba675d50e0e462abf861a9dea
Lemuel Pereira <lemuelpjr@gmail.com>
2019-05-29 15:13:35 -0300

    add first js DOM method and property examples

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+// The getElementById is a method used an specific element
+// The innerHTML property is useful for getting or replacing the content of HTML elements
+var oldText = document.getElementById('demo').innerHTML;
+console.log(oldText);
+
+document.getElementById('demo').innerHTML = 'Salut, monde ! :D';
